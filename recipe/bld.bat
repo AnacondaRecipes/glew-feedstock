352fef0d67a0ddfe87508804e839220c1de8904f
Kai Tietz <ktietz@anaconda.com>
2023-03-15 16:15:28 +0100

    update scripts and increase build-number

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,12 +2,16 @@ setlocal EnableDelayedExpansion
 
 cd build
 :: Configure using the CMakeFiles
-cmake -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" -DCMAKE_BUILD_TYPE:STRING=Release ./cmake
+cmake -G "Ninja" ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+    -DCMAKE_INSTALL_LIBDIR="lib" ^
+    -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
+    ./cmake
+
 if errorlevel 1 exit 1
 
 :: Build!
-nmake
+cmake --build . --target install
 if errorlevel 1 exit 1
 
-nmake install
-if errorlevel 1 exit 1

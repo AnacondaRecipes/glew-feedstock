2b785498ca448481b10e5f798f093d86523fb9f3
Kai Tietz <ktietz@anaconda.com>
2023-03-17 13:42:53 +0100

    tests on windows

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -5,8 +5,9 @@ cd build
 cmake  -G "NMake Makefiles" %RECIPE_DIR%/test -DCMAKE_BUILD_TYPE=Release
 nmake
 echo try to execute main application
-.\main  || cmd /k "exit /b 0"
+.\main || (echo failed && cmd /k "exit /b 0")
 
-%PREFIX%\Library\bin\glewinfo.exe
-cmd /k "exit /b 0"
+echo try to execute glewinfo
+glewinfo.exe
+cmd /K "exit /b 0"
 
